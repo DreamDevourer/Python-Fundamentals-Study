cb8cdc87a967d57243031b170e2ee18bd3749e31
Nicolas Mendes <nicolasmendes_developer@outlook.com>
2021-11-03 20:51:46 -0300

    Day 1 added

---
+++ 100 Days of Code/1-day.py
@@ -0,0 +1,5 @@
+# https://replit.com/@appbrewery/day-1-1-exercise
+# Literally print it:
+print("Day 1 - Python Print Function",
+      "\nThe function is declared like this:",
+      "\nprint('what to print')")
