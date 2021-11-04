96e89cefa2b0f981f707b4a38fec6a8d95118de5
Nicolas Mendes <nicolasmendes_developer@outlook.com>
2021-11-04 18:43:26 -0300

    Day 4 preparation

---
+++ 100 Days of Code/Preparations/4-day-PRE-random-name-list.py
@@ -0,0 +1,10 @@
+# Split string method
+import random
+names_string = input("Give me everybody's names, separated by a comma: ")
+names = names_string.split(", ")
+# 🚨 Don't change the code above 👆
+
+# Write your code below this line 👇
+
+randomName = random.choice(names)
+print(f"The choosen one to pay the bill is: {randomName}.")
