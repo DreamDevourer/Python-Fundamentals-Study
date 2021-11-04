41be85095e9ce285d917384d825987275de611d9
Nicolas Mendes <nicolasmendes_developer@outlook.com>
2021-11-04 12:18:06 -0300

    Notes on leap year script

--- 100 Days of Code/3-day-PRE-leap-year.py
+++ 100 Days of Code/3-day-PRE-leap-year.py
@@ -4,6 +4,13 @@ year = int(input("Which year do you want to check? "))
 
 #Write your code below this line 👇
 
+# Can the input be divided by 4?
+# If no, then it's not leap, if yes then:
+# Can the input be divided by 100?
+# if no then it's a leap year, if yes then:
+# Can the input be divided by 400?
+# if no then it's not a leap year, if yes then it's a leap year.
+
 def CheckLeap(year):  
   if((year % 400 == 0) or  
      (year % 100 != 0) and  
