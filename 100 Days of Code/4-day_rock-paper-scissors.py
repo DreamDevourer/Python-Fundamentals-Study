a0042c9df882b22ccdc90109cccf15fcf3cc59dc
Nicolas Mendes <nicolasmendes_developer@outlook.com>
2021-11-04 21:49:36 -0300

    Finishing day 4!

--- 100 Days of Code/4-day_rock-paper-scissors.py
+++ 100 Days of Code/4-day_rock-paper-scissors.py
@@ -1,3 +1,4 @@
+import random
 rock = '''
     _______
 ---'   ____)
@@ -25,4 +26,44 @@ scissors = '''
 ---.__(___)
 '''
 
-#Write your code below this line 👇
+# Write your code below this line 👇
+
+visualSet = {"Rock": rock, "Paper": paper, "Scissors": scissors}
+rulesSet = {"RockWinScissors": True,
+            "PaperWinRock": True, "ScissorsWinPaper": True}
+
+aiChoice = random.choice(list(visualSet.values()))
+
+print("Welcome to the game.\n0 - Rock\n1 - Paper\n2 - Scissors")
+userSet = input("Type a number: \n")
+
+if userSet == "0":
+    selectedHuman = visualSet["Rock"]
+    print(f"HUMAN:\n{selectedHuman}")
+    print(f"AI:\n{aiChoice}")
+    if aiChoice == paper:
+        print("AI wins")
+    elif aiChoice == rock:
+        print("Tie")
+    elif aiChoice == scissors:
+        print("Human wins")
+elif userSet == "1":
+    selectedHuman = visualSet["Paper"]
+    print(f"HUMAN:\n{selectedHuman}")
+    print(f"AI:\n{aiChoice}")
+    if aiChoice == paper:
+        print("Tie")
+    elif aiChoice == rock:
+        print("Human wins")
+    elif aiChoice == scissors:
+        print("AI wins")
+elif userSet == "2":
+    selectedHuman = visualSet["Scissors"]
+    print(f"HUMAN:\n{selectedHuman}")
+    print(f"AI:\n{aiChoice}")
+    if aiChoice == paper:
+        print("Human wins")
+    elif aiChoice == rock:
+        print("AI wins")
+    elif aiChoice == scissors:
+        print("Tie")
