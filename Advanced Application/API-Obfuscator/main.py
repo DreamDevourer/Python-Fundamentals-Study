a14c2de682c2b2315aa75ae86e4b7c3054851fe4
Nicolas Mendes <nicolasmendes_developer@outlook.com>
2021-12-23 15:44:45 -0300

    Adding Caesar Cipher

--- Advanced Application/API-Obfuscator/main.py
+++ Advanced Application/API-Obfuscator/main.py
@@ -2,6 +2,7 @@ import base64
 import pathlib
 import os
 import re
+import string
 from pathlib import Path
 
 
@@ -16,12 +17,32 @@ def relative_to_assets(path: str) -> Path:
     return API_PATH / Path(path)
 
 
+shiftAlpha = 4
 API_CONTENT = None
 # 🔐 Security execution READ
 
 
 def API_SEC():
     global API_CONTENT
+    global shiftAlpha
+
+    # Caesar Cipher
+    alphaCharset = string.ascii_letters
+    numCharset = string.digits
+    charsetMain = alphaCharset + numCharset
+
+    totalNum = 0
+    for i in range(len(charsetMain)):
+        totalNum += i
+
+    shiftAlpha %= totalNum
+
+    unshiftAlpha = -shiftAlpha
+
+    alphaUnshifted = None
+    alphaUnshifted = charsetMain[unshiftAlpha:] + charsetMain[:unshiftAlpha]
+    tableContentUn = str.maketrans(charsetMain, alphaUnshifted)
+
     # Security measures
     API_CONTENT = open(relative_to_assets("Data/security/API"), "r").read()
     API_DECODED = base64.b64decode(API_CONTENT.encode("utf-8"))
@@ -30,14 +51,36 @@ def API_SEC():
     API_DECODED_CLEAN = re.sub(
         r"[^A-Za-z0-9-]", "", API_DECODED.decode("utf-8"))
 
-    return API_DECODED_CLEAN
+    UNLOCKED_CONTENT = str(API_DECODED_CLEAN).translate(tableContentUn)
+
+    return UNLOCKED_CONTENT
 
 
 print("\nAdd your API here.\nNEVER CHANGE THE API KEY DIRECTLY\nFOR SECURITY REASONS!\n")
 userChange = input("Enter API key: ").strip()
 
+# Caesar Cipher
+alphaCharset = string.ascii_letters
+numCharset = string.digits
+# Combine both alphaCharset with numCharset in one variable
+charsetMain = alphaCharset + numCharset
+
+totalNum = 0
+for i in range(len(charsetMain)):
+    totalNum += i
+
+# print(totalNum)
+shiftAlpha = 4
+shiftAlpha %= totalNum
+
+alphaShifted = None
+alphaShifted = charsetMain[shiftAlpha:] + charsetMain[:shiftAlpha]
+tableContent = str.maketrans(charsetMain, alphaShifted)
+
+CIPHER_APPLIED = userChange.translate(tableContent)
+
 # Pick userChange and encode it to base64
-userChange = base64.b64encode(userChange.encode('utf-8'))
+userChange = base64.b64encode(CIPHER_APPLIED.encode('utf-8'))
 # Save userChange to "API" file
 with open(relative_to_assets('Data/security/API'), 'wb') as f:
     # Delete everything inside the file.
